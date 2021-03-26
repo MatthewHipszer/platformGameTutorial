8a4a112781e6a816797fce261e9be9143876773a
MatthewHipszer <73508557+MatthewHipszer@users.noreply.github.com>
2021-03-26 03:31:21 -0400

    Cleaned code and updated graphics slightly
    Cleaned up some code and added a way to separate the background, player layer, and foreground for level maps.

--- PlatformGameTutorial/src/net/maps/Level1Test.java
+++ PlatformGameTutorial/src/net/maps/Level1Test.java
@@ -12,17 +12,21 @@ import net.gameState.GameState;
 
 public class Level1Test {
 
-	public static BufferedImage map;
+	public static BufferedImage map, mapBackground, mapForeground;
 
 	public int x, y;
 	public int width = 2240;
 	public int height = 1484;
 
 	public Level1Test (int x, int y) {
+		//System.out.println("Map x and y: " + x + ", " + y);
+		//x and y are 0
 		this.x = x;
 		this.y = y;
 		try {
-			map = ImageIO.read(getClass().getResourceAsStream("/maps/testMap1.png"));
+			map = ImageIO.read(getClass().getResourceAsStream("/maps/map1NoBackground.png"));
+			mapBackground = ImageIO.read(getClass().getResourceAsStream("/maps/map1TestBackground1.png"));
+			//mapForeground = ImageIO.read(getClass().getResourceAsStream("/maps/map1TestForeground1.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -34,7 +38,8 @@ public class Level1Test {
 
 	public void draw (Graphics g) {
 
-			g.drawImage(map, x - (int)GameState.xOffset, y - (int)GameState.yOffset, width, height, null);
-
+		g.drawImage(mapBackground, x - ((int)GameState.xOffset/10), y - ((int)GameState.yOffset/10), width, height, null);
+		g.drawImage(map, x - (int)GameState.xOffset, y - (int)GameState.yOffset, width, height, null);
+		//g.drawImage(mapForeground, x - (int)GameState.xOffset, y - (int)GameState.yOffset, width, height, null);
 	}
 }
--- PlatformGameTutorial/src/net/maps/Map.java
+++ PlatformGameTutorial/src/net/maps/Map.java
@@ -7,7 +7,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-import net.objects.Block;
 import net.objects.CollisionLine;
 import net.objects.MovingBlock;
 
@@ -17,7 +16,6 @@ public class Map {
         WALL, FLOOR, SEMISOLIDFLOOR, DOWNWARDSLOPE, UPWARDSLOPE, CEILING
     }
 
-	private Block[][] blocks;
 	private ArrayList<MovingBlock> movingBlocks;
 	private ArrayList<CollisionLine> collisionLines;
 
@@ -41,37 +39,15 @@ public class Map {
 			movingBlocks.get(i).draw(g);
 		}
 		*/
-		//for (int i = 0; i < collisionLines.size(); i++) {
-		//	collisionLines.get(i).draw(g);
-		//}
+		for (int i = 0; i < collisionLines.size(); i++) {
+			collisionLines.get(i).draw(g);
+		}
 	}
 
 	public void loadMap(String loadPath) {
 
 		// list walls first
-		// make sure walls are bottom(y1) to top(y2)
 		collisionLines = new ArrayList<CollisionLine>();
-		/*
-		collisionLines.add(new CollisionLine(400, 100, 400, -200, LineType.WALL));				//0
-		collisionLines.add(new CollisionLine(3500, 700, 3500, 000, LineType.WALL));				//1
-		collisionLines.add(new CollisionLine(3750, 250, 3750, 000, LineType.WALL));				//2
-		collisionLines.add(new CollisionLine(4000, -490, 4000, -800, LineType.WALL));			//3
-		collisionLines.add(new CollisionLine(3500, 100, 3750, 100, LineType.FLOOR));			//4
-		collisionLines.add(new CollisionLine(0, 320, 1100, 320, LineType.FLOOR));				//5
-		collisionLines.add(new CollisionLine(100, 200, 500, 200, LineType.SEMISOLIDFLOOR));		//6
-		collisionLines.add(new CollisionLine(000, 100, 400, 100, LineType.SEMISOLIDFLOOR));		//7
-		collisionLines.add(new CollisionLine(-500, -300, 0, 320, LineType.DOWNWARDSLOPE));		//8
-		collisionLines.add(new CollisionLine(400, 100, 600, 200, LineType.DOWNWARDSLOPE));		//9
-		collisionLines.add(new CollisionLine(400, 100, 500, 200, LineType.DOWNWARDSLOPE));		//10
-		collisionLines.add(new CollisionLine(800, 250, 2000, 500, LineType.DOWNWARDSLOPE));		//11
-		collisionLines.add(new CollisionLine(2000, 500, 2500, 700, LineType.DOWNWARDSLOPE));	//12
-		collisionLines.add(new CollisionLine(2500, 700, 2750, 900, LineType.DOWNWARDSLOPE));	//13
-		collisionLines.add(new CollisionLine(200, 200, 400, 100, LineType.UPWARDSLOPE));		//14
-		collisionLines.add(new CollisionLine(2750, 900, 3500, 700, LineType.UPWARDSLOPE));		//15
-		collisionLines.add(new CollisionLine(3500, 700, 3750, 200, LineType.UPWARDSLOPE));		//16
-		collisionLines.add(new CollisionLine(3750, 200, 4000, -500, LineType.UPWARDSLOPE));		//17
-		collisionLines.add(new CollisionLine(0, -100, 3000, -100, LineType.CEILING));			//18
-		 */
 
 		//This is just an image right now
 		//It would be nice if everything could be moved into this
@@ -125,10 +101,6 @@ public class Map {
 			e.printStackTrace();
 		}
 
-	}
-
-	public Block[][] getBlocks() {
-		return blocks;
 	}
 
 	public ArrayList<MovingBlock> getMovingBlocks() {
